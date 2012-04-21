ed8653418bd588534071a65ec2bf16fa35dd1858
Mark Scholtz <markscholtz@gmail.com>
2012-04-21 13:09:15 +0200

    Implement some kind of naming conventions
    - Still a work in progress
    - Inspired by https://github.com/unboxed/Javascript-Style-Guide

--- mus/compiler.js
+++ mus/compiler.js
@@ -2,29 +2,30 @@
 // Helper functions
 //************************************************
 
-var endTime = function (time, expr) {
+var end_time = function (time, expr) {
     // Base case
     if(expr.tag === 'note' || expr.tag === 'rest') {
         return time + expr.dur;
     }
 
     if(expr.tag === 'seq') {
-        return endTime(time, expr.left) + endTime(time, expr.right);
+        return end_time(time, expr.left) + end_time(time, expr.right);
     }
 
     if(expr.tag === 'par') {
-        return Math.max(endTime(time, expr.left), endTime(time, expr.right));
+        return Math.max(end_time(time, expr.left), end_time(time, expr.right));
     }
 };
 
-var convertPitch = function (pitch) {
-  var note = pitch[0];
-  var octave = pitch[1];
-  return 12 + 12 * octave + letterPitch(note);
+var convert_pitch = function (pitch) {
+  var note   = pitch[0],
+      octave = pitch[1];
+
+  return 12 + 12 * octave + letter_pitch(note);
 }
 
-var letterPitch = function (note_letter) {
-  var note_dictionary =
+var letter_pitch = function (note_letter) {
+  var _note_dictionary =
       { c  : 0,
         d  : 2,
         e  : 4,
@@ -33,49 +34,51 @@ var letterPitch = function (note_letter) {
         a  : 9,
         b  : 11 };
 
-  return note_dictionary[note_letter];
+  return _note_dictionary[note_letter];
 }
 
 //************************************************
 // Compiler
 //************************************************
 
-var compile = function(musexpr, to_midi) {
-    var noteArray = [];
+var compile = function(mus_expression, to_midi) {
+    var _get_notes,
+        _note_array = [];
+
     to_midi = typeof to_midi !== 'undefined' ? to_midi : false;
 
-    var getNotes = function(node, start) {
-        var nextStart = 0;
+    var _get_notes = function(node, start) {
+        var _nextStart = 0;
 
         // Base case
         if(node.tag === 'note' || node.tag === 'rest') {
             node.start = start;
             if(to_midi && node.pitch) {
-                node.pitch = convertPitch(node.pitch);
+                node.pitch = convert_pitch(node.pitch);
             }
             start += node.dur;
-            noteArray.push(node);
-            return noteArray;
+            _note_array.push(node);
+            return _note_array;
         }
 
         if(node.tag === 'seq') {
-          nextStart = endTime(start, node.left);
+          _nextStart = end_time(start, node.left);
         }
 
         if(node.tag === 'par') {
           // need to capture start time for next seq and then use the current start for
           // all subsequent par notes
-          nextStart = start;
+          _nextStart = start;
         }
 
-        getNotes(node.left, start);
-        getNotes(node.right, nextStart);
+        _get_notes(node.left, start);
+        _get_notes(node.right, _nextStart);
 
-        return noteArray;
+        return _note_array;
     };
 
-    noteArray = getNotes(musexpr, 0);
-    return noteArray;
+    _note_array = _get_notes(mus_expression, 0);
+    return _note_array;
 };
 
 //************************************************
