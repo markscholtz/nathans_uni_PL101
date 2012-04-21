de3608e6516e4a9dde905d4b4da53a09cd51be9e
Mark Scholtz <markscholtz@gmail.com>
2012-04-21 11:41:56 +0200

    Add support for rests

--- mus/compiler.js
+++ mus/compiler.js
@@ -4,7 +4,7 @@
 
 var endTime = function (time, expr) {
     // Base case
-    if(expr.tag === 'note') {
+    if(expr.tag === 'note' || expr.tag === 'rest') {
         return time + expr.dur;
     }
 
@@ -28,7 +28,7 @@ var compile = function(musexpr) {
         var nextStart = 0;
 
         // Base case
-        if(node.tag === 'note') {
+        if(node.tag === 'note' || node.tag === 'rest') {
             node.start = start;
             start += node.dur;
             noteArray.push(node);
@@ -59,6 +59,10 @@ var compile = function(musexpr) {
 // "Tests"
 //************************************************
 
+//************************************************
+// "Tests: sequential"
+//************************************************
+
 var sequential_melody_input =
     { tag: 'seq',
       left:
@@ -69,6 +73,57 @@ var sequential_melody_input =
        { tag: 'seq',
          left: { tag: 'note', pitch: 'c4', dur: 500 },
          right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+var sequential_melody_expected_output = [
+    { tag: 'note', pitch: 'a4', dur: 250, start: 0 },
+    { tag: 'note', pitch: 'b4', dur: 250, start: 250 },
+    { tag: 'note', pitch: 'c4', dur: 500, start: 500 },
+    { tag: 'note', pitch: 'd4', dur: 500, start: 1000 } ];
+
+sequential_melody_output = compile(sequential_melody_input);
+console.log('----> SEQUENTIAL melody output:');
+console.log(sequential_melody_expected_output);
+console.log('');
+console.log('----> SEQUENTIAL melody expected output:');
+console.log(sequential_melody_output);
+console.log('');
+console.log('');
+console.log('');
+
+//************************************************
+// "Tests: sequential with rests"
+//************************************************
+
+var sequential_melody_rests_input =
+    { tag: 'seq',
+      left:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'rest', dur: 100 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+var sequential_melody_rests_expected_output = [
+    { tag: 'note', pitch: 'a4', dur: 250, start: 0 },
+    { tag: 'note', dur: 100, start: 250 },
+    { tag: 'note', pitch: 'c4', dur: 500, start: 350 },
+    { tag: 'note', pitch: 'd4', dur: 500, start: 850 } ];
+
+sequential_melody_rests_output = compile(sequential_melody_rests_input);
+console.log('----> SEQUENTIAL melody rests output:');
+console.log(sequential_melody_rests_output);
+console.log('');
+console.log('----> SEQUENTIAL melody rests expected output:');
+console.log(sequential_melody_rests_expected_output);
+console.log('');
+console.log('');
+console.log('');
+
+//************************************************
+// "Tests: parallel"
+//************************************************
 
 var parallel_melody_input =
     { tag: 'seq',
@@ -81,27 +136,11 @@ var parallel_melody_input =
          left: { tag: 'note', pitch: 'd3', dur: 500 },
          right: { tag: 'note', pitch: 'f4', dur: 250 } } };
 
-var sequential_melody_expected_output = [
-    { tag: 'note', pitch: 'a4', dur: 250, start: 0 },
-    { tag: 'note', pitch: 'b4', dur: 250, start: 250 },
-    { tag: 'note', pitch: 'c4', dur: 500, start: 500 },
-    { tag: 'note', pitch: 'd4', dur: 500, start: 1000 } ];
-
 var parallel_melody_expected_output = [
     { tag: 'note', pitch: 'c3', dur: 250, start: 0 },
     { tag: 'note', pitch: 'g4', dur: 500, start: 0 },
     { tag: 'note', pitch: 'd3', dur: 500, start: 500 },
     { tag: 'note', pitch: 'f4', dur: 250, start: 500 } ];
-
-sequential_melody_output = compile(sequential_melody_input);
-console.log('----> SEQUENTIAL melody output:');
-console.log(sequential_melody_expected_output);
-console.log('');
-console.log('----> SEQUENTIAL melody expected output:');
-console.log(sequential_melody_output);
-console.log('');
-console.log('');
-console.log('');
 
 parallel_melody_output = compile(parallel_melody_input);
 console.log('----> PARALLEL melody output:');
